b7ff8c5285421fba6c2d318227c72065bdd5ff75
Jotun <jayden.alonzoestrada@gmail.com>
2025-07-05 19:29:41 -0600

    Updating and Cleaning

--- 100PrisonersProblem.cpp
+++ 100PrisonersProblem.cpp
@@ -11,148 +11,102 @@ The 100 Prisoners Riddle
 
 using namespace std;
 
-void RunExp(int[], int, int, int, int&, int&);
-void RandRunExp(int[], int, int, int, int&, int&, int&, int&);
-void PrintArray(int[], int);
+void RunExp(int[], int, int, int, int&, int&, int&);
+void RandRunExp(int[], int, int, int, int&, int&, int&);
 void Scramble(int[], int);
-void clearScreen();
 
 // Define Parameters
-#define iters 1000000
+#define iters 10000
 #define NumbOfBoxes 100
 #define NumbOfTries 50
-#define MaxLoopSize 0
+#define MaxLoopSize 0 // WIP
 
-int main(int argc, char* argv[]){  
-  // clearScreen();
+int main(int argc, char* argv[]){
   cout << endl;
 
   // Init of Vars
   random_device Gen;
-  int Successes, Fails, Wins = 0, Losses = 0, heldSucc = 0, heldFail = 0, 
-    Room[NumbOfBoxes];
-  string None;
-
-  // Scramble Room
-  Scramble(Room, NumbOfBoxes);
-
-  // Visually verify scamble of Room
-  // PrintArray(Room, NumbOfBoxes);
-
-  // RunExp Once
-  // RunExp(Room, NumbOfBoxes, NumbOfTries, MaxLoopSize, Successes, Fails);
-  // printf("Successes = %i :: Fails = %i Check #1\n", Successes, Fails);
-
-  // Full Run of Experiment 
-  // Scramble(Room, NumbOfBoxes);
-  // RunExp(Room, NumbOfBoxes, NumbOfTries, MaxLoopSize, Successes, Fails);
-  // printf("Successes = %i :: Fails = %i Check #2\n", Successes, Fails);
+  int exp_failed, Wins = 0, Losses = 0, held_successes = 0, held_fails = 0, Room[NumbOfBoxes];
 
   // Run Experiment in Series
+  // Optimized Method
   for (int i = 0; i < iters; i++){
     Scramble(Room, NumbOfBoxes);
-    RunExp(Room, NumbOfBoxes, NumbOfTries, MaxLoopSize, Successes, Fails);
-    // printf("Successes = %i :: Fails = %i #%i\n", Successes, Fails, i+1);
-    if (Fails > 0){
-      Losses++;
-    } else {
-      Wins++;
-    }
+    RunExp(Room, NumbOfBoxes, NumbOfTries, MaxLoopSize, exp_failed, held_successes, held_fails);
+    if (exp_failed > 0){Losses++;} else {Wins++;}
   }
-  printf("Alg: Wins = %i :: Losses = %i\n", Wins, Losses);
-  Wins = 0;
-  Losses = 0;
-  // Run Random Experiment in Series
+
+  // Print Algorithm Stats
+  printf("Alg: Successes = %i :: Failures = %i\n", held_successes, held_fails);
+  printf("Alg: Wins = %i :: Losses = %i :: Win Percentage = %.2f%%\n", Wins, Losses, (Wins * 100.0) / iters);
+
+  // Reset Stats
+  exp_failed=0; Wins=0; Losses=0; held_fails=0; held_successes=0;
+
+  // Random Method
   for (int i = 0; i < iters; i++){
     Scramble(Room, NumbOfBoxes);
-    RandRunExp(Room, NumbOfBoxes, NumbOfTries, MaxLoopSize, Successes, Fails, heldSucc, heldFail);
-    // printf("heldSuccesses = %i :: heldFails = %i #%i\n", heldSucc, heldFail, i+1);
-    if (Fails > 0){
-      Losses++;
-    } else {
-      Wins++;
-    }
-  }
-  printf("heldSuccesses = %i :: heldFails = %i\n", heldSucc, heldFail);
-  printf("Rand: Wins = %i :: Losses = %i\n", Wins, Losses);
+    RandRunExp(Room, NumbOfBoxes, NumbOfTries, MaxLoopSize, exp_failed, held_successes, held_fails);
+    if (exp_failed > 0){Losses++;} else {Wins++;}}
+  
+  // Print Random States
+  printf("Rand: Successes = %i :: Fails = %i\n", held_successes, held_fails);
+  printf("Rand: Wins = %i :: Losses = %i :: Win Percentage = %.2f%%\n", Wins, Losses, (Wins * 100.0) / iters);
 
   cout << endl;
   return 0;
 }
 
-void RunExp(int array[], int size, int tries, int maxloopsize, int &success, int &fails){
+// Runs Experiment with Optimization
+void RunExp(int array[], int size, int tries, int maxloopsize, int &fails, int &holdSuccess, int &holdFails){
   int tmp,tmper,largestLoop;
-  success=0;fails=0;
-  string none;
-  // cout << "Where" << endl;
+  fails=0;
   for (int i = 0; i < size; i++){
-    // cout << "COnfuse" << endl;
     tmp = i+1;
     tmper = tmp;
-    // cout << tmp << endl;
     for (int j = 0; j < tries; j++){
-      // cout << "What" << endl;
-      // printf("Number = %i :: Value = %i\n", tmp, array[tmp-1]);
-      // getline(cin, none);
       if (array[tmp-1] != tmper){
         tmp = array[tmp-1];
       } else if (array[tmp-1] == tmper) {
-        // printf("Answer Found\n");
-        success++;
+        holdSuccess += 1;
         break;
       } if (j == tries-1) {
         fails++;
+        holdFails += 1;
       }
     }
   }
-  // printf("Successes = %i :: Fails = %i\n", success, fails);
 }
 
-void RandRunExp(int array[], int size, int tries, int maxloopsize, int &success, int &fails, int &holdSuccess, int &holdFails){
+// Run Experiment with Random
+void RandRunExp(int array[], int size, int tries, int maxloopsize, int &fails, int &holdSuccess, int &holdFails){
   int tmp,tmper,largestLoop;
   random_device Gen;
   mt19937 rng(Gen());
   uniform_int_distribution<int> dist(1, size);
-  success=0;fails=0;
-  string none;
-  // cout << "Where" << endl;
+  fails=0;
   for (int i = 0; i < size; i++){
-    // cout << "COnfuse" << endl;
     tmp = dist(rng);
     tmper = i+1;
-    // cout << tmp << endl;
     for (int j = 0; j < tries; j++){
-      // cout << "What" << endl;
-      // printf("Number = %i :: Value = %i\n", tmp, array[tmp-1]);
-      // getline(cin, none);
       if (array[tmp-1] != tmper){
         tmp = dist(rng);
       } else if (array[tmp-1] == tmper) {
-        // printf("Answer Found\n");
-        success++;
+        holdSuccess += 1;
         break;
       } if (j == tries-1) {
         fails++;
+        holdFails += 1;
       }
     }
   }
-  holdFails += fails;
-  holdSuccess += success;
-  // printf("holdSuccesses = %i :: holdFails = %i\n", holdSuccess, holdFails);
 }
 
-void PrintArray(int array[], int size){
-  printf("Contents of Room array:\n");
-  for (int i = 0; i < size; i++){
-    printf("Array[%i] == %i\n", i, array[i]);
-  }
-}
-
+// Randomize the Array
 void Scramble(int array[], int size){
   random_device Gen;
-  mt19937 rng(Gen());  // Better random number generator
-  
-  // Initialize array with values 1 to size
+  mt19937 rng(Gen());
+
   for (int i = 0; i < size; i++) {
     array[i] = i + 1;
   }
@@ -161,19 +115,8 @@ void Scramble(int array[], int size){
   for (int i = size - 1; i > 0; i--) {
     uniform_int_distribution<int> dist(0, i);
     int j = dist(rng);
-    // Swap array[i] and array[j]
     int temp = array[i];
     array[i] = array[j];
     array[j] = temp;
   }
-}
-
-void clearScreen()
-{
-    // use "cls" in windows and "clear" command in Mac and Linux
-    #ifdef _WIN32
-        system("clS");
-    #else
-        system("clear");
-    #endif
 }
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # 100PrisonerProblem
-coding up the 100PrisonerProblem
+## Written by Jaynuke79
+
+I originally heard of the 100 Prisoner Problem through a Veritasium video, but only after having a heated intellectual debate with my statistic's teacher, did I take it upon myself to show him emperically.
+
+### Usage
+#### - 1. Clone Repo
+#### - 2. Define Parameters: 
+##### - `iters` | Amount of Total Iterations
+##### - `NumbOfBoxes` | Amount of Total Boxes
+##### - `NumbOfTries` | Amount of Tries per Prisoner
+##### - `MaxLoopSize` | (WIP) Caps the largest Cycle of Numbers
+#### - 3. Run the command, `make`, in the terminal in the project root
+
+### My original code was written in C++ and the python script was written by Claude
